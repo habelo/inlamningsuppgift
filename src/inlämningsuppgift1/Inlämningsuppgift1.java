0932ab248b52f63fac8eb5bf60359ecc0492d673
Jonas Stjerna <jonasstj@gmail.com>
2017-10-02 20:56:03 +0200

    Första steg, test upp till cloud

--- src/inlämningsuppgift1/Inlämningsuppgift1.java
+++ src/inlämningsuppgift1/Inlämningsuppgift1.java
@@ -1,5 +1,15 @@
 package inlämningsuppgift1;
 
 public class Inlämningsuppgift1 {
-
+	public static void main(String[] args) {
+		
+	}
+	
+	public void uppBygg() {
+		Djur sixten = new Hund("Sixten", 5);
+		Djur dogge = new Hund("Dogge", 10);
+		Djur venus= new Katt("Venus", 5);
+		Djur ove= new Katt("Ove", 3);
+		Djur hypno= new Katt("Hypno", 1);
+	}
 }
